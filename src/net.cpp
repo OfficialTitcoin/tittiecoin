2571b71249422025b71cc71a2580fb6f5f6a09df
Tittiecoin <info@tittiecoin.com>
2014-12-12 20:23:18 +0100

    Seednodes added

--- src/net.cpp
+++ src/net.cpp
@@ -1143,7 +1143,8 @@ void MapPort()
 // The first name is used as information source for addrman.
 // The second name should resolve to a list of seed addresses.
 static const char *strDNSSeed[][2] = {
-
+    {"seed1", "178.62.32.188"},
+	{"seed2", "178.62.32.202"},    
 };
 
 void ThreadDNSAddressSeed(void* parg)
