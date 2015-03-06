599432ac40a0c8b7cac7bb89fe20169a0fe007a3
raroun <raroun@tittiecoin.com>
2015-03-06 09:28:35 +0100

    Reduced the amount of total coins to 2806003200

--- src/main.h
+++ src/main.h
@@ -32,7 +32,7 @@ static const unsigned int MAX_BLOCK_SIGOPS = MAX_BLOCK_SIZE/50;
 static const unsigned int MAX_ORPHAN_TRANSACTIONS = MAX_BLOCK_SIZE/100;
 static const int64 MIN_TX_FEE = 1 * COIN;
 static const int64 MIN_RELAY_TX_FEE = MIN_TX_FEE;
-static const int64 MAX_MONEY = 5612006400 * COIN; // 
+static const int64 MAX_MONEY = 2806003200 * COIN; // 
 inline bool MoneyRange(int64 nValue) { return (nValue >= 0 && nValue <= MAX_MONEY); }
 static const int COINBASE_MATURITY = 50;
 // Threshold for nLockTime: below this value it is interpreted as block number, otherwise as UNIX timestamp.
